f0c7394451edeaef3b7dffd298df93677e03b0f6
ax <114583570+bths-ax@users.noreply.github.com>
2023-03-27 09:29:21 -0400

    oh god java swing again

---
+++ src/Arc.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+public class Arc extends Shape {
+	public Arc() {
+		super();
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(getColor());
+		g.drawArc(getMinX(), getMinY(), getWidth(), getHeight() * 2, 0, 180);
+	}
+}
